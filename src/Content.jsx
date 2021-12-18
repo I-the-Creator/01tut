4f85471b4d082b64debe35b2316c772e7fa3440a
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-18 14:01:00 +0100

    add few handleClick functions

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,14 +4,33 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
     }
+    
+    const handleClick = () => {
+        console.log('You clicked it');
+    }
 
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+    }
+
+    const handleClick3 = (event) => {
+        console.log(event.target.innerText);
+    }
+    
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It</button>
+
+            {/* if we don't add anonymous function as a wrapper for 'handle function with parameters' it will be invoked immediately  */}
+            <button onClick={() => handleClick2('Andrew')}>Click It</button>
+            {/* we can pass the 'event' object to the 'handleClick3' function */}
+            <button onClick={(event) => handleClick3(event)}>Click It</button>
         </main>
     );
 };
 
 export default Content;
+
