87e084a1e4ed349a1ddf76af4e6c20d5d5e9aeb6
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-21 16:38:49 +0100

    add the function for handle checkboxes on click or double click;  add item inline style

--- src/Content.jsx
+++ src/Content.jsx
@@ -20,16 +20,34 @@ const Content = () => {
         }
     ]);
 
+    const handleCheck = (id) => {
+        // console.log(`key: ${id}`);
+        /* iterate on items and check if item.id === id that got from input then create new object
+         equal to original (using spread operator) and flip the current checked property value (!item.checked),
+         otherwise return the current 'item' w/o changes
+         */
+        const listItems = items.map((item) => item.id === id ? { ...item,
+        checked: !item.checked } : item);
+        // send new listItem array to state via setItems
+        setItems(listItems);
+        // save state value to localStorage
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
     return (
         <main>
             <ul>
-                {items.map((item) => (
+                {items.map((item) => (  // iterate on items array
                     <li className="item" key={item.id}>
                         <input 
                             type="checkbox"
+                            onChange={() => handleCheck(item.id)}
                             checked={item.checked}
                         />
-                        <label>{item.item}</label>
+                        <label
+                            style={(item.checked) ? { textDecoration: 'line-through'} : null}
+                            onDoubleClick={() =>handleCheck(item.id)}
+                        >{item.item}</label>
                         <FaTrashAlt 
                             role="button" 
                             tabIndex="0" 
