d85161993ef13fbddd288d0e7b5f41b58289fe6b
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-23 14:54:39 +0100

    remove handler functions logic,  state and default state value

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,51 +1,6 @@
-import { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
  
-const Content = () => {
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            checked: true,   // check sign enabled
-            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-        },
-        {
-            id: 2,
-            checked: false,
-            item: "Item 2"
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "Item 3"
-        }
-    ]);
-
-    const handleCheck = (id) => {
-        // console.log(`key: ${id}`);
-        /* iterate on items and check if item.id === id that got from input then create new object
-         equal to original (using spread operator) and flip the current checked property value (!item.checked),
-         otherwise return the current 'item' w/o changes
-         */
-        const listItems = items.map((item) => item.id === id ? { ...item,
-        checked: !item.checked } : item);
-        // send new listItem array to state via setItems
-        setItems(listItems);
-        // save state value to localStorage
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    }
-
-    const handleDelete = (id) => {
-        /*use filter method - it creates new filtered array 
-            filter out the items that have item.id == id,
-            other items added to new array = listItems
-        */
-        const listItems = items.filter((item) => item.id !== id);
-        // send new listItem array to state via setItems
-        setItems(listItems);
-        // save state value to localStorage
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    }
-
+const Content = ({ items, handleCheck, handleDelete}) => {
     return (
         <main>
             {/* if items exist show the list */}
