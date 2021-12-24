cf8317e1c6a5aa418e59bfec1c6cbd48db3e6a22
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-24 11:36:41 +0100

    title prop add to Header, get it from App.js

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Header = () => {
+// Destructure props that got from APP.js 
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>Groceries List</h1>
+            <h1>{title}</h1>
         </header>
     );
 };
+
+// Default prop if it's not got for some reason form parent component
+Header.defaultProps = {
+    title: "Default Title"
+}
 
 export default Header;
 
