f330d122f81bcee0211c181e5d5eb6e87878c745
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-24 11:38:52 +0100

    items list layout moved to separate component ItemsList

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,37 +1,19 @@
-import { FaTrashAlt } from 'react-icons/fa';
+import ItemsList from './ItemsList';
  
-const Content = ({ items, handleCheck, handleDelete}) => {
+const Content = ({ items, handleCheck, handleDelete }) => {
     return (
         <main>
             {/* if items exist show the list */}
             {items.length ? (
-            <ul>
-                {items.map((item) => (  // iterate on items array
-                    <li className="item" key={item.id}>
-                        <input 
-                            type="checkbox"
-                            onChange={() => handleCheck(item.id)}
-                            checked={item.checked}
-                        />
-                        <label
-                            style={(item.checked) ? { textDecoration: 'line-through' } : null}
-                            // to pass in the item.id the handleClick needs to be wrapped inside anonymous function
-                            onDoubleClick={() =>handleCheck(item.id)}
-                        >{item.item}</label>
-                        <FaTrashAlt 
-                            // to pass in the item.id the handleDelete needs to be wrapped inside anonymous function
-                            onClick={() => handleDelete(item.id)}
-                            role="button" 
-                            tabIndex="0" 
-                        />
-                    </li>
-
-                ))}
-            </ul>
-            ) : (
-                /* if the list is empty show the message */
-                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-            )}
+                <ItemsList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+                ) : (
+                    /* if the list is empty show the message */
+                    <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+                )}
         </main>
     );
 };
