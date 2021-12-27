87cc2b3ff109a5e53c7eb551f54f6c26ea04337f
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-27 16:06:10 +0100

    useRef hook add to focus on AddItem input after submiting

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,7 +1,11 @@
 import { FaPlus } from 'react-icons/fa';
+import { useRef } from 'react';
 
 // get the props from App.js
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+    // to set the focus back to input after we click '+' button(add the item)
+    const inputRef = useRef();
+
     return (
         // on form submit call the handleSubmit function
         <form className="addForm" onSubmit={handleSubmit}>
@@ -10,6 +14,7 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
 
             <input 
                 autoFocus
+                ref={inputRef}  /* link to input ref */
                 id='addItems'
                 type='text'
                 placeholder='Add Item'
@@ -22,6 +27,10 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
             <button
                 type='submit'
                 aria-label='Add Item'
+                /*to set the focus back to INPUT after clicking '+' button
+                useRef always has .current property
+                 - focus set to element with inputRef in attribute*/
+                onClick={() => inputRef.current.focus()}
             >
                 <FaPlus />
             </button>
